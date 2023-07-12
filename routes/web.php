db75e23289195eb98a7e866d7950bfa0a7c06da2
akuutauf <taufikhidayat09121@gmail.com>
2023-07-12 15:04:15 +0700

    Initial Project, Templating

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route Pages
+Route::get('/', [PageController::class, 'dashboard'])->name('dashboard.page');
+
+// Route for Auth
+Route::get('/register', [AuthController::class, 'register'])->name('register.page');
+Route::get('/login', [AuthController::class, 'login'])->name('login.page');
+Route::get('/forgot-password', [AuthController::class, 'forgot_password'])->name('forgot-password.page');
